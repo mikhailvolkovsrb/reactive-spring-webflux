8838b2510c16b625cf9126a78f2b6887a1e5e348
Mikhail Volkov <leopard@inbox.ru>
2024-07-24 17:25:51 +0200

    Movies-Review-Service: New features and tests

--- movies-review-service/src/main/java/com/reactivespring/exceptionhandler/GlobalErrorHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/exceptionhandler/GlobalErrorHandler.java
@@ -8,6 +8,7 @@ import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebExceptionHandler;
 import reactor.core.publisher.Mono;
 
 @Component
